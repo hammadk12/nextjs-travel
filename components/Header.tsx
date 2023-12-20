015f823cb82d9ce1b6db4c51de04a584428ac07d
hammadk12 <131798537+hammadk12@users.noreply.github.com>
2023-12-20 17:04:31 -0500

    first commit 12/20

---
+++ components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header>
+            <h1 className='text-3xl'>Hello</h1>
+        </header>
+    );
+};
+
+export default Header;
