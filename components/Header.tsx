1bfcef44ec1becd3bb8bed56c78ccf2f7f0de1e0
hammadk12 <131798537+hammadk12@users.noreply.github.com>
2023-12-21 21:13:08 -0500

    2nd commit 12/21

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,9 +2,33 @@ import React from 'react';
 
 const Header = () => {
     return (
-        <header>
-            <h1 className='text-3xl'>Hello</h1>
-        </header>
+        <div className="navbar bg-base-100 fixed top-0 w-full shadow-md">
+            <div className="flex-1">
+                <a className="btn btn-ghost text-xl prose">RoamHub</a>
+            </div>
+            <div className="flex-none gap-2">
+                <div className="form-control">
+                    <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto" />
+                </div>
+                <div className="dropdown dropdown-end">
+                    <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+                        <div className="w-10 rounded-full">
+                            <img alt="Tailwind CSS Navbar component" src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                        </div>
+                    </div>
+                    <ul tabIndex={0} className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
+                        <li>
+                            <a className="justify-between">
+                                Profile
+                                <span className="badge">New</span>
+                            </a>
+                        </li>
+                        <li><a>Settings</a></li>
+                        <li><a>Logout</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
     );
 };
 
